cea6450654ab7e7ff3ffcc212b44374f216f92cf
Aaron Gordon <asgordon96@gmail.com>
2015-10-03 13:02:03 -0500

    Make the webcam streaming work. Start location services.

---
+++ STS Hackathon/Assets/CameraFeed.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CameraFeed : MonoBehaviour {
+	public WebCamTexture back;
+	public RawImage rawimage;
+	public LocationInfo location;
+
+	// Use this for initialization
+	void Start () {
+		back = new WebCamTexture (480, 325);
+		rawimage = GetComponent<RawImage>();
+		rawimage.texture = back;
+		back.Play ();
+
+		if (!Input.location.isEnabledByUser) {
+			// user doesn't have location services enabled
+			return;
+		}
+		Input.location.Start ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (Input.location.status);
+		//print (Input.location.lastData.latitude);
+		//print (Input.location.lastData.longitude);
+	}
+}
---
+++ STS Hackathon/Assets/DeviceCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeviceCamera : MonoBehaviour {
+	public WebCamTexture back;
+	public Renderer renderer;
+	public Skybox skybox;
+	public WebCamDevice[] devices;
+	// Use this for initialization
+	void Start () {
+		devices = WebCamTexture.devices;
+		print (devices[0].name);
+		back = new WebCamTexture (640, 320, 30);
+		renderer = GetComponent<Renderer>();
+		renderer.material.mainTexture = back;
+		back.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
